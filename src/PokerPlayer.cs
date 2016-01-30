42ff13c6c0b8386f1f1fed33f9c73cac7a2c7161
Tibor Kanyó <kanyo.tibi@gmail.com>
2016-01-30 10:34:07 +0100

    50% eséllyel all in vagy csak tét tartása

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,15 +1,22 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
+        private static readonly Random rnd = new Random();
+
 		public static readonly string VERSION = "Default C# folding player";
 
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			return 0;
+		    int valueToCall = (int) gameState["current_buy_in"] - (int) gameState["players"]["in_action"]["bet"];
+
+            //50% eséllyel csak tartjuk a tétet, egyébként pedig all in :D 
+		    if (rnd.Next(1) == 1)
+		        return valueToCall;
+			return 1000000;
 		}
 
 		public static void ShowDown(JObject gameState)

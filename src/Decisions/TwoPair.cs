46e329cb6fbfbcc9cfa74f50d291ee56b02ac971
Nagy János <johnny.1101@gmail.com>
2016-01-30 16:00:12 +0100

    TwoPair

--- src/Decisions/TwoPair.cs
+++ src/Decisions/TwoPair.cs
@@ -1,39 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Nancy.Simple.Interface;
-
-namespace Nancy.Simple.Decisions
+﻿namespace Nancy.Simple.Decisions
 {
-    public class TwoPair : IDecisionLogic
+    public class TwoPair : AbstractPair
     {
-        public string GetName()
+        public override string GetName()
         {
             return "TwoPair";
         }
 
-        public int? MakeADecision(GameState gameState)
+        public override int? MakeADecision(GameState gameState)
         {
-            //sosem játszik, nem tartozik még hozzá logika
+            var myCards = GetMyCards(gameState);
 
 
             return null;
-        }
-
-        private string HasAPair(JToken cards)
-        {
-            //var cardType = "";
-            //var tmpCards = new List<JToken>();
-            //tmpCards.Any(c => c["rank"] != card["rank"]);
-
-            //foreach (var card in cards)
-            //{
-
-            //    break;
-            //}
-
-            //return cardType;
-
-            return "";
         }
     }
 }
